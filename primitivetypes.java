ae1e718133890af52fd300ad7a3e48acbc465078
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-23 20:43:50 +0530

    primitive data types

---
+++ primitivetypes.java
@@ -0,0 +1,24 @@
+public class primitivetypes {
+    public static void main(String[] args){
+        int a = 2;
+        char b = 'A';
+        boolean c = true;
+        byte d = -128;
+        short e = 32000;
+        float f = 2.5f;
+        double g = 99.99;
+        Long h = 123456789L;
+        boolean i = false;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        System.out.println(i);
+
+    }
+}
