f77bf341803317ef27870a4a4eca178ed6bdf473
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-23 12:37:33 +0530

    basic operation in java

---
+++ operators.java
@@ -0,0 +1,20 @@
+public class firstprogram {
+    public static void main(String[] args){
+        int a = 20;
+        int b = 10;
+        int c = a+b;
+        int d = a-b;
+        int f = a*b;
+        int g = a/b;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(f);
+        System.out.println(g);
+    }
+}
+
+
+
+
