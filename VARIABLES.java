a888093a4a935f16d4d442dc3b93a3f6c21b14d8
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-24 20:35:27 +0530

    variable and its type in java

---
+++ VARIABLES.java
@@ -0,0 +1,11 @@
+public class VARIABLES {                           // variables are container to store the value or data
+    int y = 25;                                   // here y is instances variable which declared inside the class 
+    static int z = 26;                  // here z is static variable that use static keyword and it is also declared inside class
+    public static void main(String[] args) {
+        int x = 23;                             // x is a local variable which is inside the main function
+        System.out.println(x);
+        VARIABLES v = new VARIABLES();
+        System.out.println(v.y);
+        System.out.println(v.z);
+    }
+}
