967365ee525c8b516d22d13636fb6e8c3b234289
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-24 23:12:34 +0530

    assignment operator

---
+++ assignment.java
@@ -0,0 +1,18 @@
+public class assignment {
+    public static void main(String[] args) {
+        int x= 2;
+        int y = 4;
+        int w = 6;
+        int z = 8;
+        x+=2;
+        y-=2;
+        w*=4;
+        z/=2;
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(w);
+        System.out.println(z);
+
+
+    }
+}
