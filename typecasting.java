5a075b90b642cfa74ff821bd8b99b962dca89b8a
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-24 20:06:39 +0530

    typecasting in java

---
+++ typecasting.java
@@ -0,0 +1,10 @@
+
+
+public class typecasting {                    //type casting is the process in java that covert big type to small type 
+    public static void main(String[] args) {
+        double dumber = 3.2f;
+        int number = (int) dumber;
+        System.out.print(number);
+
+    }
+}
