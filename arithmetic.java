5153329e05083d16ada15b0a157d037d8d834761
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-24 22:09:10 +0530

    arithmetic operator

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+public class arithmetic {
+    public static void main(String[] args){
+        int num1 = 2;
+        int num2 = 10;
+        System.out.println(num1+num2);                          // Addition
+        System.out.println(num1-num2);                         //Subtraction
+        System.out.println(num1*num2);                        //Multiplication
+        System.out.println(num1/num2);                       //Divison
+        System.out.println(num1%num2);                      //Remainder(modulus)
+    }
+}
