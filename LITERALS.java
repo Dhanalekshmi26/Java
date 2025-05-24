30fe41430d4972e34efe5696ac98604def6ec701
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-24 18:32:40 +0530

    literals in java

---
+++ LITERALS.java
@@ -0,0 +1,21 @@
+public class LITERALS {                                   //literals are the direct values use to give in program
+    public static void main(String[] args){
+        int a = 22;                                       //integer literal
+        float b = 3.5f;                                   //float literal
+        double c = 9.8;                                   //double literal
+        char d = 'd';                                     //char literal
+        String e = "meow";                                //string literal
+        boolean g = false;                                //boolean literal
+        boolean p = true;                                 
+        String msg = null;                                //null literal
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(g);
+        System.out.println(p);
+        System.out.println(msg);
+
+    }
+}
