401ba0cedee4fb3f3834edde46ee27488ec49583
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-24 19:20:53 +0530

    typeconversion in java

---
+++ typeconversion.java
@@ -0,0 +1,7 @@
+public class typeconversion {      //type conversion is the automatic process in java that covert small type to big type
+    public static void main(String[] args) {
+        int number = 20;                                //int -> double conversion
+        double dumber = number;
+        System.out.println(dumber);
+                }
+}                                          
