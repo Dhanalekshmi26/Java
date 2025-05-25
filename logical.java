07fa558bbb17dae966e998ebfc95a32402c8410d
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-25 10:48:32 +0530

    logical opeartor in java

---
+++ logical.java
@@ -0,0 +1,9 @@
+public class logical {
+    public static void main(String[] args){
+        int a = 30;
+        boolean b = false;
+        System.out.println(a>30 && a<30);         //logical and 
+        System.out.println(a>30 || a==30);       //logical or
+        System.out.println(!b);                 //logical not
+    }
+}
