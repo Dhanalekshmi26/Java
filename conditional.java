712e7a701d72fcf7d1c9ca4cf0f1d9ac08863ca2
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-25 12:29:22 +0530

    conditional operator in java

---
+++ conditional.java
@@ -0,0 +1,7 @@
+public class conditional {                              //conditional operator is like a if else condition in one line
+    public static void main(String[] args) {
+        int age = 18;
+        String adult = (age>=18)?"Yes":"No";
+        System.out.println(adult);
+    }
+}
