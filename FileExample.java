204d243639ca644bf504be77bcaeb3715466df16
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-25 21:09:13 +0530

    file handling in java

---
+++ FileExample.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+public class FileExample {
+    public static void main(String[] args) {
+        File file = new File("demo.txt");
+        if (file.exists()) {
+            System.out.println("File exists");
+        } else {
+            System.out.println("File doesn't exist");
+        }
+    }
+}
