656810c23b1e576d809540783983609a5980172a
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-26 17:11:48 +0530

    a type of pattern example

---
+++ patternexample.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class patternexample{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int  java   =   sc.nextInt();
+        int  cpp    =   sc.nextInt();
+        int  python =   sc.nextInt();
+        for(int i=1;i<=32;i++){
+            System.out.print("=");
+        }System.out.println();
+        System.out.println("java       "+java);
+        System.out.println("cpp        "+cpp);
+        System.out.println("python     "+python);
+        for(int j=1;j<=32;j++){
+            System.out.print("=");
+        }
+
+                            
+    }
+}
+
+
+
+
+
+/*java         100 
+cpp            065 
+python         050 */
