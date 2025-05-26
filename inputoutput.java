32184b7e14b070e70d422daea155f5a3a9627dd2
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-26 22:49:17 +0530

    input ouput taking in java

---
+++ inputoutput.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class inputoutput {
+    
+public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the 1stnumbers: ");
+       int a = sc.nextInt();
+       System.out.print("Enter the 2ndnumber: ");
+       int b = sc.nextInt();
+       System.out.print("Enter the 3rdnumber: ");
+       int c = sc.nextInt();
+       
+       System.out.println("Your 1st number is: "+a);
+       System.out.println("Your 2nd number is: "+b);
+       System.out.println("Your 3rd number is: "+c);
+       
+    }}
+
