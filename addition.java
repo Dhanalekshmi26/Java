c7de9a65ef48273f2cf91707ef1d47130f681c8f
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-26 22:37:48 +0530

    example of function in java

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    void addNumbers(int a, int b){
+        System.out.println(a+b);
+    }
+    public static void main(String[] args) {
+        addition add = new addition();
+        add.addNumbers(2, 9);
+    }
+}
