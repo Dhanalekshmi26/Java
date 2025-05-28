45fe3a294991febd4b8547432f1e63cc8dd51538
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-28 21:48:26 +0530

    this question is With Argument With Return

---
+++ MathOps.java
@@ -0,0 +1,12 @@
+/*Q7: Write a class MathOps with a method square(int number) that takes an integer and returns its square. 
+Call it and print the result. */
+public class MathOps {
+    int square(int a){
+        return a*a;
+
+    }
+    public static void main(String[] args) {
+        MathOps mo = new MathOps();
+        mo.square(2);
+    }                                               //this question is With Argument With Return
+}
