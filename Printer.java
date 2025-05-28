77660fb2eb688bcebbd3a9fb1088e682ffd90169
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-28 21:47:38 +0530

    this question is With Argument Without Return

---
+++ Printer.java
@@ -0,0 +1,14 @@
+
+
+/*Q6: Write a class Printer with a method printMessage(String msg) that accepts a string as an 
+argument and prints it. */
+public class Printer {
+    void printMessage(String msg){
+        System.out.println(msg);
+    }
+    public static void main(String[] args) {
+        Printer pm = new Printer();
+        pm.printMessage("hiii");                //this question is With Argument Without Return
+        
+    }
+}
