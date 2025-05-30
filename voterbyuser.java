6742c1315d097f5285816c85347ea798285e4875
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-05-30 22:48:17 +0530

    user enter the age and check

---
+++ voterbyuser.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class voterbyuser {
+    boolean isEligible(int age) {
+        return age >= 18;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Voter v = new Voter();
+        System.out.print("Enter 1st Age: ");
+        int age1 = sc.nextInt();
+        System.out.print("Enter 2ndt Age: ");
+        int age2 = sc.nextInt();
+        System.out.println("Age " + age1 + ": Eligible? " + v.isEligible(age1));
+        System.out.println("Age " + age2 + ": Eligible? " + v.isEligible(age2));
+    }
+    }
+
