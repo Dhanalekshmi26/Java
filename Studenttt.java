c8754ecf0e403bc710f782faaa7fa7c8a630e2d0
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-01 21:27:42 +0530

    encapsulation in java

---
+++ Studenttt.java
@@ -0,0 +1,19 @@
+public class Studenttt{
+
+    private int age;  //hiding data 
+
+    public void setage(int a){        //visible method for setting age 
+        age = a ;
+    }
+    public int getage(){             //visible method for getting/printing age
+        return age;
+    }
+        
+    
+public static void main(String[] args){
+    Studenttt st = new Studenttt();
+    st.setage(21);
+    System.out.println("age of student is : "+st.getage());
+
+}
+}//encapsulation : wrapping data and methods together to keep data protected from unauthorised access 
