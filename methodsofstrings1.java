4c90738009cc488b7af493df7d94eb9f63080b01
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-04 21:02:45 +0530

    string methods

---
+++ methodsofstrings1.java
@@ -0,0 +1,10 @@
+public class methodsofstrings1{
+    public static void main(String[] args){
+        String txt = "windows laptop";
+        System.out.println(txt.indexOf("p"));
+        System.out.println(txt.lastIndexOf("o"));
+        System.out.println(txt.replace("windows","mac"));
+        
+    }
+}
+
