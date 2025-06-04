7933b5a07f93186f1cb16a7e69f4e6aacd5d0628
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-04 19:33:08 +0530

    check starts with and ends with

---
+++ startend.java
@@ -0,0 +1,7 @@
+public class startend {
+    public static void main(String[] args) {
+        String txt = "windows laptop";
+        System.out.println(txt.startsWith("wi"));
+        System.out.println(txt.endsWith("top"));
+    }
+}
