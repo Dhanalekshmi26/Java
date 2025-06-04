9448da7b30ef699e2558325096f46acf38b35066
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-04 17:14:16 +0530

    return a character based on index

---
+++ stringcharcter.java
@@ -0,0 +1,6 @@
+public class stringcharcter{
+    public static void main(String[] args){
+        String txt = "windows laptop";
+        System.out.println("index is: " + txt.charAt(8));  //print the letter on the index u typed
+                       }
+                                       }                   
