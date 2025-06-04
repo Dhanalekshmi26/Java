ff5a77ae3cbb47aa65168d6bd1972d1178615e0d
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-04 17:15:51 +0530

    convert the lower string to uppercase and upper string to lower case

---
+++ upperlower.java
@@ -0,0 +1,7 @@
+public class upperlower{
+    public static void main(String[] args){
+        String txt = "windows laptop";
+        System.out.println("My name in capital letters be like: "+txt.toUpperCase());      //converted txt to uppercase
+        System.out.println("My name in small letters be like: "+txt.toLowerCase());        ////converted txt to lowercase
+          }
+              }      
