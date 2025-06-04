fba7193b0d0f28c25cee9958e4228e047b91334e
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-04 17:16:46 +0530

    get the length of string

---
+++ lengthofstring.java
@@ -0,0 +1,7 @@
+public class lengthofstring{
+    public static void main(String[] args){
+        String txt = "windows laptop";
+        int LEN = txt.length();
+        System.out.println("My length of string or u can say length of name is: "+LEN);  //print the length of txt
+    }
+}     
