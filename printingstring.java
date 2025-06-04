c30328eb9834dcf762afd296396a8e14e194722b
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-04 17:11:08 +0530

    printing string in java

---
+++ printingstring.java
@@ -0,0 +1,6 @@
+public class printingstring{
+    public static void main(String[] args){
+        String txt = "windows laptop";
+        System.out.println("I am: "+txt);                      //print text
+    }
+}
