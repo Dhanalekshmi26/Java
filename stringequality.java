cc29f33203431f21e6ac2dc5d4442d945d0c9908
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-04 17:12:50 +0530

    check the equality of string

---
+++ stringequality.java
@@ -0,0 +1,7 @@
+public class stringequality{
+    public static void main(String[] args) {
+        String a = "apple";
+        String b = "oranage";
+        System.out.println(a.equals(b));
+    }
+}
