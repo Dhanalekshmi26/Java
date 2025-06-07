f31f2d2a84f37cc8d56ccf762a592a486a10d35b
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-07 12:54:06 +0530

    array in java

---
+++ Arrayexample.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+public class Arrayexample {
+    
+    public static void main(String[] args){
+        int []marks = {90,80,96,85,99,100};
+        System.out.println("overall marks he got-> "+Arrays.toString(marks));
+        String []names = {"jeevan","dwani","dhyan","achu"};
+        System.out.println("babies now: "+Arrays.toString(names));
+
+        
+
+    }
+}
