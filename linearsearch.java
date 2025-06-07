e9c8f45ab36c45a9bf19447021465fdd9d7d6459
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-07 22:57:56 +0530

    linear search

---
+++ linearsearch.java
@@ -0,0 +1,20 @@
+public class linearsearch {
+    public static void main(String[] args) {
+        int[] numbers = {10, 20, 30, 40, 50};
+        int target = 300;
+
+        boolean found = false;
+
+        for(int i = 0; i < numbers.length; i++) {
+            if(numbers[i] == target) {
+                System.out.println("Element found at index: " + i);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found) {
+            System.out.println("Element not found!");
+        }
+    }
+}
