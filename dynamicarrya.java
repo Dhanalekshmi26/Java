f3cfbcda22424e3beed29b37dc6e5f434519cdc3
Dhanalekshmi26 <dhanalekshmi2622@gmail.com>
2025-06-16 20:25:24 +0530

    array creation according to user's wish

---
+++ dynamicarrya.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class dynamicarrya{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the count of marks you want to display: ");
+        int size = sc.nextInt();
+
+        int []marks = new int[size];
+        for(int i=0;i<size;i++){
+            System.out.print("Your entered marks "+(i)+": ");
+            marks[i] = sc.nextInt();
+        }
+        for(int nums:marks){
+            System.out.print(" "+nums);
+        }
+    }
+}
